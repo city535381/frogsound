9aae5096992401a26557fe3dfbb7c8b0b3a18835
city535381 <134923496+city535381@users.noreply.github.com>
2023-05-29 17:39:22 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://city535381.github.io/frogsound/model.json';
 
 
 function preload() {
